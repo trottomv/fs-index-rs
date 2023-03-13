37915d5021cafb511d32ed53a58ecbcaf3554424
Matteo Vitali <matteo@20tab.com>
2023-03-13 10:15:47 +0100

    Add search handler

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use std::env;
 use std::path::Path;
 use walkdir::WalkDir;
 mod config;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::{BufReader, BufWriter};
 
 #[derive(Hash, Debug, PartialEq, Deserialize, Serialize)]
 struct FSItem {
@@ -28,7 +29,7 @@ fn set_project(path: &String) -> &'static str {
     ""
 }
 
-fn set_key_words(path: &String) -> Vec<String> {
+fn set_key_words(path: &String, project: &String, file_name: &String) -> Vec<String> {
     // Tag an item with keywords
     let mut key_words: Vec<String> = vec![];
     for key_word in config::KEY_WORDS {
@@ -36,6 +37,8 @@ fn set_key_words(path: &String) -> Vec<String> {
             key_words.push(key_word.to_string());
         }
     }
+    key_words.push(project.to_string());
+    key_words.push(file_name.to_string());
     key_words
 }
 
@@ -52,7 +55,9 @@ fn index_directory(path: String, ignore_patterns: &[&str]) -> Vec<FSItem> {
         if !entry.file_type().is_file() {
             continue;
         }
+        let file_name = entry.file_name().to_str().unwrap().to_string();
         let file_path = entry.path().to_str().unwrap().to_string();
+        let project = (set_project(&file_path)).to_string();
         let fs_item = FSItem {
             file_extension: entry
                 .path()
@@ -60,35 +65,67 @@ fn index_directory(path: String, ignore_patterns: &[&str]) -> Vec<FSItem> {
                 .unwrap_or_default()
                 .to_string_lossy()
                 .to_string(),
-            file_name: entry.file_name().to_str().unwrap().to_string(),
+            file_name: file_name.to_string(),
             file_path: file_path.clone(),
             file_size: entry.metadata().unwrap().len(),
-            key_words: set_key_words(&file_path),
-            project: (set_project(&file_path)).to_string(),
+            key_words: set_key_words(&file_path, &project, &file_name),
+            project: project,
         };
         fs_items.push(fs_item)
     }
     fs_items
 }
 
+fn fts_search(query: &str) {
+    // Full Text Search by given query string
+    let file = File::open(Path::new(config::OUTPUT_FILENAME)).unwrap();
+    let reader = BufReader::new(file);
+    let fs_items: Vec<FSItem> = serde_json::from_reader(reader).unwrap();
+    let re = Regex::new(&format!("{}", query)).unwrap();
+    let mut matching_items: Vec<&FSItem> = vec![];
+    let binding = re.to_string().to_lowercase();
+    let query_words: Vec<&str> = binding.split_whitespace().collect();
+    let _fs_items_response: Vec<&FSItem> = fs_items
+        .iter()
+        .filter(|&fs_item| {
+            query_words
+                .iter()
+                .all(|&word| fs_item.key_words.contains(&(*word).to_string()))
+        })
+        .collect();
+    matching_items.extend(_fs_items_response);
+    for item in matching_items {
+        println!("{:?}", item.file_path);
+    }
+}
+
+fn build_index(path: &str) {
+    let fs_items = index_directory(path.to_string(), &config::IGNORE_PATTERNS);
+    let output_file = File::create(config::OUTPUT_FILENAME).expect("Unable to create file");
+    let bw = BufWriter::new(output_file);
+    serde_json::to_writer(bw, &fs_items).expect("Failed writing :(");
+    println!(
+        "[INFO] {} items saved into {}",
+        fs_items.len(),
+        config::OUTPUT_FILENAME
+    );
+}
+
 fn main() {
-    // build file system index
+    // The main function
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("[ERROR] No path provided.");
-        eprintln!("Help: cargo run <path>");
+        println!("[ERROR] No argument provided.");
+        eprintln!("Help to build index: cargo run <path>");
+        eprintln!("Help to search: cargo run search 'phrase to search'");
         std::process::exit(1);
+    } else if args[1] == "search" {
+        // Run search
+        fts_search(&args[2]);
     } else {
+        // Run build file system index
         let path = Path::new(&args[1]).to_string_lossy();
-        let fs_items = index_directory(path.to_string(), &config::IGNORE_PATTERNS);
-        let output_file = File::create(config::OUTPUT_FILENAME).expect("Unable to create file");
-        let bw = BufWriter::new(output_file);
-        serde_json::to_writer(bw, &fs_items).expect("Failed writing :(");
-        println!(
-            "[INFO] {} items saved into {}",
-            fs_items.len(),
-            config::OUTPUT_FILENAME
-        );
+        build_index(&path);
     }
 }
 
@@ -105,10 +142,14 @@ mod tests {
 
     #[test]
     fn test_set_key_words() {
-        let path = String::from("/path/to/index/backend/Dockerfile");
-        let key_words = set_key_words(&path);
+        let path = String::from("/path/to/myproject/backend/Dockerfile");
+        let project = String::from("myproject");
+        let file_name = String::from("Dockerfile");
+        let key_words = set_key_words(&path, &project, &file_name);
         assert!(key_words.contains(&String::from("backend")));
         assert!(key_words.contains(&String::from("docker")));
+        assert!(key_words.contains(&String::from("myproject")));
+        assert!(key_words.contains(&String::from("Dockerfile")));
     }
 
     #[test]
@@ -116,9 +157,7 @@ mod tests {
         let path = String::from("./test-data");
         let ignore_patterns = ["ignore.txt"];
         let fs_items = index_directory(path, &ignore_patterns);
-
         assert_eq!(fs_items.len(), 2);
-
         let _expected_items = [
             FSItem {
                 file_extension: String::from("txt"),
